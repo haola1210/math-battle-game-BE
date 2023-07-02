a151f95c1eede17697a5c6c55577b0f6fb9cd4ea
haola1210 <haola1210@gmail.com>
2023-07-03 01:19:49 +0700

    fix redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-import * as redisStore from 'cache-manager-redis-store';
-import { Module, CacheModule } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
+import { Module, CacheModule, CacheStore } from '@nestjs/common';
 import { EventsModule } from './features/events/events.module';
 import UsersModule from './features/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -12,22 +12,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
       useFactory: async (config: ConfigService) => {
-        console.log(redisStore);
-        const client = redisStore.create().getClient();
-
-        client.on('error', (err) => console.log('Redis Client Error', err));
-        client.on('ready', () => {
-          const response = client.ping();
-          console.log(`PING to redis server: ${response}`);
-        });
-
-        return {
-          store: {
-            create: () => client,
+        const store = await redisStore({
+          socket: {
+            host: config.get('REDIS_HOST'),
+            port: +config.get('REDIS_PORT'),
           },
-          host: config.get('REDIS_HOST'),
-          port: Number(config.get('REDIS_PORT')),
-          password: config.get('REDIS_PWD'),
+          password: config.get('REDIS_PASSWORD'),
+        });
+        return {
+          store: store as unknown as CacheStore,
           ttl: null,
         };
       },
