e4811a5ab743e660e30c34e718cbdadbd8aab3e8
MACBOOK <macbook@MACBOOKs-MacBook-Pro-2.local>
2023-07-03 22:46:10 +0700

    resoleve conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
-import * as redisStore from 'cache-manager-redis-store';
-import { Module, CacheModule } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-store';
+import { Module, CacheModule, CacheStore } from '@nestjs/common';
 import { EventsModule } from './features/events/events.module';
 import UsersModule from './features/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { APP_GUARD } from '@nestjs/core';
-import { ProxyGuard } from './features/auth/guards/proxy.guard';
 import { AuthModule } from './features/auth/auth.module';
 
 @Module({
@@ -22,27 +20,15 @@ import { AuthModule } from './features/auth/auth.module';
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       //@ts-ignore
       useFactory: async (config: ConfigService) => {
-        const client = redisStore
-          .create({
+        const store = await redisStore({
+          socket: {
             host: config.get('REDIS_HOST'),
-            port: Number(config.get('REDIS_PORT')),
-            password: config.get('REDIS_PWD'),
-          })
-          .getClient();
-
-        client.on('error', (err) => console.log('Redis Client Error', err));
-        client.on('ready', () => {
-          const response = client.ping();
-          console.log(`PING to redis server: ${response}`);
-        });
-
-        return {
-          store: {
-            create: () => client,
+            port: +config.get('REDIS_PORT'),
           },
-          host: config.get('REDIS_HOST'),
-          port: Number(config.get('REDIS_PORT')),
-          password: config.get('REDIS_PWD'),
+          password: config.get('REDIS_PASSWORD'),
+        });
+        return {
+          store: store as unknown as CacheStore,
           ttl: null,
         };
       },
